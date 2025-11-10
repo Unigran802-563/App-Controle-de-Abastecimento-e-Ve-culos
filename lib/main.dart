f995a77bdb460927a5abe2e857827083a72d5f48
Dev01idxcode <dev01@idxcode.com.br>
2025-11-10 17:11:58 -0400

    Inicio do Projeto

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Importe o arquivo gerado
+
+void main() async {
+  // Garante que os Widgets do Flutter foram inicializados
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Controle de Abastecimento',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Text("App Inicializado!"), // Vamos mudar isso na próxima etapa
+    );
+  }
+}
